f27efb8d293687f4eb4c727118c1e7d67c6a1cb5
atulrpatil <atulpatil_21589@yahoo.co.in>
2021-07-11 20:33:51 +0530

    Mutil Thread

---
+++ Customer.java
@@ -0,0 +1,24 @@
+class Customer {
+	int amount = 10000;
+
+	synchronized void withdraw(int amount) {
+		System.out.println("going to withdraw...");
+
+		if (this.amount < amount) {
+			System.out.println("Less balance; waiting for deposit..."+this.amount);
+			try {
+				wait();
+			} catch (Exception e) {
+			}
+		}
+		this.amount -= amount;
+		System.out.println("withdraw completed..."+this.amount);
+	}
+
+	synchronized void deposit(int amount) {
+		System.out.println("going to deposit...");
+		this.amount += amount;
+		System.out.println("deposit completed... "+this.amount);
+		notify();
+	}
+}
